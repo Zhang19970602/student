19ee8ab460742d2c7c4f15b561b396915ac15b73
zhang <1602523229@qq.com>
2020-06-16 11:17:09 +0800

    开发签退功能

---
+++ Signout.java
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2020-06-16 11:16:38
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-06-16 11:20:13
+*/
+
+public class Signout {
+    private String name;
+
+}
