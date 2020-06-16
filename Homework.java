ecab438aa553f3c1db97f687fcf71ea96acf589e
zhang_01 <zhang_01@qq.com>
2020-06-16 15:23:56 +0800

    rabase code

--- Homework.java
+++ Homework.java
@@ -1,11 +1,15 @@
 /*
 * @Author: Marte
+<<<<<<< HEAD
 * @Date:   2020-06-16 11:51:07
 * @Last Modified by:   Marte
-* @Last Modified time: 2020-06-16 15:07:36
+* @Last Modified time: 2020-06-16 15:29:30
 */
 
 public class Homework {
     //在家开发
      private String content;
+
+    //Modified in company
+    private int id;
 }
