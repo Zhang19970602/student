3d7d29db4d24357df802a2458dd4b43abc98cd29
zhang_01 <zhang_01@qq.com>
2020-06-16 10:38:21 +0800

    在公司开发一部分画图功能

--- Paint.java
+++ Paint.java
@@ -2,9 +2,10 @@
 * @Author: Marte
 * @Date:   2020-06-16 10:31:18
 * @Last Modified by:   Marte
-* @Last Modified time: 2020-06-16 10:32:07
+* @Last Modified time: 2020-06-16 10:37:08
 */
 
 public class Paint {
+    //公司开发画图部分功能
     public  int a;
 }
