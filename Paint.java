4f25f0ba0d9ddbcd9fe6421fe59f549d41d04f79
zhang <1602523229@qq.com>
2020-06-16 10:38:08 +0800

    在家开发请假和画图剩余功能

---
+++ Paint.java
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2020-06-16 10:35:46
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-06-16 10:36:46
+*/
+
+public class Paint {
+    //在加开发画图剩余功能
+    private String color;
+}
